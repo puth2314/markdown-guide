8df36b343752419eada4f2fafa696cab1a8296d4
Puthearath Aim <u7227681@anu.edu.augit config --global user.email u7227681@anu.edu.augit config --global user.email u7227681@anu.edu.au>
2022-06-19 16:48:25 +1000

    added convert-to.sh a bash scripts that converts to markdown to html, pdf, and slide

---
+++ scripts/convert-to.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/bash
+
+[ -z $1 ] && echo -e "\nFAILED: EMPTY ARG." && exit 0
+
+if [ $1 = "html" ]; then
+    OFILE="markdown-guide.html"
+    OFLAGS="-s --mathjax"
+elif [ $1 = "pdf" ]; then
+    OFILE="markdown-guide.pdf"
+    OFLAGS="--pdf-engine=pdflatex --resource-path=.;src"
+elif [ $1 = "slide" ]; then
+    OFILE="markdown-slide.pdf"
+    OFLAGS="-t beamer -V theme:Luebeck --resource-path=.;src"
+else
+    echo -e "\nFAILED: INVALID ARG."
+    exit 0
+fi
+
+IFILE="markdown-guide.md"
+
+echo $(pandoc src/${IFILE} -o build/${OFILE} ${OFLAGS})
+echo "SUCCESS: Converted ${IFILE} to ${OFILE}."
