2882e3367743b55007c04dec5a179b5f633978d9
Puthearath Aim <u7227681@anu.edu.augit config --global user.email u7227681@anu.edu.augit config --global user.email u7227681@anu.edu.au>
2022-06-20 22:31:15 +1000

    updated script to convert to html slides

--- scripts/README.md
+++ scripts/README.md
@@ -1,3 +1,3 @@
 # How To Run
 
-In a bash termimal (e.g. `Git Bash`), in the root directory, enter `bash scripts/convert-to.sh html`, to convert `markdown-guide.md` to `markdown-guide.html`. Replace `html` for `pdf` or `slide` in the argument to convert to LaTeX (`pdflatex`) or Beamer respectively.
+In a bash termimal (e.g. `Git Bash`), in the root directory, enter `bash scripts/convert-to.sh html guide`, to convert `markdown-guide.md` to `markdown-guide.html`. Replace `html` for `pdf` and/or replace `guide` for `slide` in the arguments to convert to pdf guide and/or slides respectively.
--- scripts/convert-to.sh
+++ scripts/convert-to.sh
@@ -1,22 +1,44 @@
 #! /usr/bin/bash
 
-[ -z $1 ] && echo -e "FAILED: EMPTY ARG." && exit 0
+[ -z $1 ] || [ -z $2 ] && echo -e "FAILED: EMPTY ARGS." && exit 0
 
-if [ $1 = "html" ]; then
-    OFILE="markdown-guide.html" # -t slidy; for html slides
+if [ $1 = "html" ] && [ $2 = "guide" ]; then
     OFLAGS="-s --mathjax"
-elif [ $1 = "pdf" ]; then
-    OFILE="markdown-guide.pdf"
-    OFLAGS="--pdf-engine=pdflatex --resource-path=.;src" # last arg is because of some weird pandoc behavior; such as making and removing a temp file in the directory
-elif [ $1 = "slide" ]; then
-    OFILE="markdown-slide.pdf"
-    OFLAGS="-t beamer -V theme:Luebeck" 
+    FFLAGS=""
+elif [ $1 = "html" ] && [ $2 = "slide" ]; then
+    OFLAGS="-s --mathjax -t slidy"
+    FFLAGS=""
+elif [ $1 = "pdf" ] && [ $2 = "guide" ]; then
+    OFLAGS="--pdf-engine=pdflatex"
+    FFLAGS="--resource-path=.;src" # this arg is because of some weird pandoc behavior
+elif [ $1 = "pdf" ] && [ $2 = "slide" ]; then
+    OFLAGS="-t beamer"
+    FFLAGS="--resource-path=.;src"
 else
-    echo -e "FAILED: INVALID ARG." && exit 0
+    echo -e "FAILED: INVALID ARGS." && exit 0
 fi
 
-IFILE="markdown-guide.md"
+BFLAGS="-C --bibliography=misc/references.bib --csl=misc/ieee-with-url.csl" # remove last arg if you do not have a .csl file
 
-BFLAGS="-C --bibliography=misc/references.bib --csl=misc/ieee-with-url.csl" # remove the last arg if you do not have a .csl file
+echo $(pandoc src/markdown-${2}.md -o build/markdown-${2}.${1} ${OFLAGS} ${FFLAGS} ${BFLAGS})
 
-echo $(pandoc src/${IFILE} -o build/${OFILE} ${OFLAGS} ${BFLAGS})
+
+
+# if [ $1 = "html" ]; then
+#     OFILE="markdown-guide.html" # -t slidy; for html slides
+#     OFLAGS="-s --mathjax"
+# elif [ $1 = "pdf" ]; then
+#     OFILE="markdown-guide.pdf"
+#     OFLAGS="--pdf-engine=pdflatex --resource-path=.;src" 
+# elif [ $1 = "slide" ]; then
+#     OFILE="markdown-slide.pdf"
+#     OFLAGS="-t beamer -V theme:Luebeck" 
+# else
+#     echo -e "FAILED: INVALID ARG." && exit 0
+# fi
+
+# IFILE="markdown-guide.md"
+
+# BFLAGS="-C --bibliography=misc/references.bib --csl=misc/ieee-with-url.csl" # remove the last arg if you do not have a .csl file
+
+# echo $(pandoc src/markdown-${2}.md -o build/markdown-${2}.${1} ${OFLAGS} ${BFLAGS})
